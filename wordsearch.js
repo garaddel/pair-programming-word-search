8168554dc415b0d983b483cd122ff65bb8c998c4
garaddel <garaddel@gmail.com>
2023-06-07 19:37:57 -0400

    Passing word search solver

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+  
+    for (const row of letters) {   // Iterate over each row of letters
+      const verticalWord = [];
+  
+      for (const column of row) {  // Iterate over each column of the current row
+        verticalWord.push(column); // Add the character to the vertical word array
+      }
+  
+      // Convert the vertical array to a string
+      const verticalJoin = verticalWord.join('');
+  
+      // Checks to see if the word is present (horizontally or vertically)
+      if (horizontalJoin.includes(word) || verticalJoin.includes(word)) {
+        return true;
+      }
     }
-}
-
-module.exports = wordSearch
+  
+    // Word not found
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
